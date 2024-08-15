90beefe0f7cc2c1cc8897a8a369fdb58a229a375
Imre Nemethµ <imi.nemeth@gmail.com>
2024-08-15 20:00:45 +0200

    Add Project

---
+++ main.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/pion/webrtc/v3"
+	"github.com/rs/cors"
+)
+
+var (
+	peerConnection *webrtc.PeerConnection
+	dataChannels   []*webrtc.DataChannel
+	mu             sync.Mutex
+	coordinates    = [2]int{0, 0}
+)
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/offer", handleOffer)
+	mux.HandleFunc("/ice", handleICE)
+	handler := cors.Default().Handler(mux)
+	log.Println("Starting server on :3001")
+	log.Fatal(http.ListenAndServe(":3001", handler))
+}
+
+func handleOffer(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var offer struct {
+		Type string `json:"type"`
+		SDP  string `json:"sdp"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&offer); err != nil {
+		http.Error(w, "Failed to decode offer", http.StatusBadRequest)
+		return
+	}
+
+	if offer.Type != "offer" || offer.SDP == "" {
+		http.Error(w, "Invalid offer", http.StatusBadRequest)
+		return
+	}
+
+	api := webrtc.NewAPI(webrtc.WithSettingEngine(webrtc.SettingEngine{}))
+
+	config := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
+
+	var err error
+	peerConnection, err = api.NewPeerConnection(config)
+	if err != nil {
+		http.Error(w, "Failed to create PeerConnection", http.StatusInternalServerError)
+		return
+	}
+
+	peerConnection.OnDataChannel(func(d *webrtc.DataChannel) {
+		log.Printf("Data channel '%s' received\n", d.Label())
+
+		d.OnOpen(func() {
+			log.Printf("Connection Open")
+		})
+
+		d.OnClose(func() {
+			deleteCandidateFromMemory(d)
+
+		})
+
+		mu.Lock()
+		dataChannels = append(dataChannels, d)
+		mu.Unlock()
+
+		d.OnMessage(func(msg webrtc.DataChannelMessage) {
+
+			var direction = string(msg.Data)
+			var arr []int
+			_ = json.Unmarshal(msg.Data, &arr)
+			coordinates = [2]int(arr)
+			log.Printf("Message received from data channel '%s': %s\n", d.Label(), direction)
+			broadcastMessage()
+		})
+	})
+
+	sdp := webrtc.SessionDescription{Type: webrtc.SDPTypeOffer, SDP: offer.SDP}
+	if err := peerConnection.SetRemoteDescription(sdp); err != nil {
+		log.Printf("error setting remote description: %s\n", err)
+		http.Error(w, "Failed to set remote description", http.StatusInternalServerError)
+		return
+	}
+
+	answer, err := peerConnection.CreateAnswer(nil)
+	if err != nil {
+		http.Error(w, "Failed to create answer", http.StatusInternalServerError)
+		return
+	}
+
+	if err := peerConnection.SetLocalDescription(answer); err != nil {
+		http.Error(w, "Failed to set local description", http.StatusInternalServerError)
+		return
+	}
+
+	response, err := json.Marshal(answer)
+	if err != nil {
+		http.Error(w, "Failed to marshal answer", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
+
+func handleICE(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var candidate struct {
+		Candidate     string `json:"candidate"`
+		SDPMid        string `json:"sdpMid"`
+		SDPMLineIndex int    `json:"sdpMLineIndex"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&candidate); err != nil {
+		http.Error(w, "Failed to decode ICE candidate", http.StatusBadRequest)
+		return
+	}
+
+	iceCandidate := webrtc.ICECandidateInit{
+		Candidate:     candidate.Candidate,
+		SDPMid:        strPtr(candidate.SDPMid),
+		SDPMLineIndex: uint16Ptr(uint16(candidate.SDPMLineIndex)),
+	}
+
+	if err := peerConnection.AddICECandidate(iceCandidate); err != nil {
+		log.Printf("error adding ICE candidate: %s\n", err)
+		http.Error(w, "Failed to add ICE candidate", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func broadcastMessage() {
+	//mu.Lock()
+	//defer mu.Unlock()
+
+	for _, channel := range dataChannels {
+		coorJson, _ := json.Marshal(coordinates)
+
+		if err := channel.SendText(string(coorJson)); err != nil {
+			log.Printf("Failed to send message on data channel '%s': %s", channel.Label(), err)
+		}
+	}
+}
+
+func deleteCandidateFromMemory(d *webrtc.DataChannel) {
+	for index, channel := range dataChannels {
+		if channel.ID() == d.ID() {
+			mu.Lock()
+			dataChannels = append(dataChannels[:index], dataChannels[index+1:]...)
+			mu.Unlock()
+			log.Printf("Candidate %d deleted", d.ID())
+		}
+	}
+}
+
+func strPtr(s string) *string {
+	return &s
+}
+
+func uint16Ptr(i uint16) *uint16 {
+	return &i
+}
